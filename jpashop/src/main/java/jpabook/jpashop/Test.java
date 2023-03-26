eb4231d3aee1385412620480ab9241924fd224a2
JWbase <jungwoo1002@gmail.com>
2023-03-26 21:51:55 +0900

    - 롬복 테스트

---
+++ jpashop/src/main/java/jpabook/jpashop/Test.java
@@ -0,0 +1,9 @@
+package jpabook.jpashop;
+
+public class Test {
+    public static void main(String[] args) {
+        LombokTest lombokTest = new LombokTest();
+
+        lombokTest.getLombok();
+    }
+}
