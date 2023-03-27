b2e80d0d24d986e83cc8dd5ee4d6707eda41c81e
JWbase <jungwoo1002@gmail.com>
2023-03-27 10:32:12 +0900

    - DevTools 테스트

--- jpashop/src/main/java/jpabook/jpashop/Test.java
+++
@@ -1,9 +0,0 @@
-package jpabook.jpashop;
-
-public class Test {
-    public static void main(String[] args) {
-        LombokTest lombokTest = new LombokTest();
-
-        lombokTest.getLombok();
-    }
-}
